dba31120aed42c4ae2d6555704a0d20ee874b283
cjh64 <66864682+cjh64@users.noreply.github.com>
2024-07-01 16:33:55 +0800

    added footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<style>
+#footer {
+    background-color: black;
+            color: white;
+            text-align: center;
+            padding: 10px 0;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+	}
+</style>
+</head>
+<body>
+
+<div id="footer">
+<p>&copy; Pharmacy Inventory Management System</p>
+	</div>
+	
+	</html>
+	</body>
