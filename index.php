8159f2246ee743940c886e59f841cd23f4b608d4
Eutopiassssss <eunicegohxintoong@gmail.com>
2024-07-01 17:30:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,9 @@ if (!isset($_SESSION['username'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<?php 
+    <?php
     include ("topnav.php");
     ?>
     <meta charset="UTF-8">
@@ -24,39 +25,7 @@ if (!isset($_SESSION['username'])) {
         }
 
 
-        .menubar {
-            background-color: #ffffff;
-            color: #333;
-            text-align: center;
-            font-size: 18px;
-            padding: 10px 20px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-        }
 
-        .menubar .left,
-        .menubar .right {
-            display: flex;
-            align-items: center;
-        }
-
-        .menubar .left a,
-        .menubar .right a {
-            color: #333;
-            text-decoration: none;
-            padding: 10px 15px;
-            border-radius: 5px;
-            transition: background-color 0.3s, color 0.3s;
-            margin: 0 5px;
-        }
-
-        .menubar .left a:hover,
-        .menubar .right a:hover {
-            background-color: whitesmoke;
-            color: black;
-        }
 
         .container {
             position: relative;
@@ -105,13 +74,14 @@ if (!isset($_SESSION['username'])) {
             opacity: 0.7;
         }
     </style>
-   
+
 </head>
 
 <body>
 
     <div class="container">
-        <img src="https://plus.unsplash.com/premium_photo-1682129923019-a2343cbcfddf?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="mainpage-image">
+        <img src="https://plus.unsplash.com/premium_photo-1682129923019-a2343cbcfddf?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="mainpage-image">
         <div class="text-overlay">
             <h2>Welcome to the Pharmacy Inventory System</h2>
             <p>Use the navigation menu to manage your inventory.</p>
@@ -121,7 +91,7 @@ if (!isset($_SESSION['username'])) {
         </div>
     </div>
 
-    <?php 
+    <?php
     include ("footer.php");
     ?>
 </body>
